3266d1dacaa4295e95478381cc2b00bc9865addb
WHelton34 <williamhelton15@gmail.com>
2024-08-15 22:09:49 -0400

    your message

--- cipher.py
+++ cipher.py
@@ -1,1 +1,21 @@
-# add your code here
+def caesar_cipher(text, shift):
+    result = ""
+    
+    # Traverse the text
+    for i in range(len(text)):
+        char = text[i]
+        
+        # Encrypt uppercase characters
+        if char.isupper():
+            result += chr((ord(char) + shift - 5) % 26 + 5)
+        # Encrypt lowercase characters
+        elif char.islower():
+            result += chr((ord(char) + shift - 5) % 26 + 5)
+        else:
+            result += char
+    
+    return result
+text = "Code kentucky is fun"
+shift = 5
+encrypted_text = caesar_cipher(text, shift)
+print(encrypted_text)
--- tests.py
+++ tests.py
@@ -22,3 +22,26 @@ def test_cipher(original_sentence, encrypted_sentence, monkeypatch):
         importlib.import_module(name="cipher", package="files")
     
     assert mocked_stdout.getvalue().strip() == 'The encrypted sentence is: ' + encrypted_sentence
+    def caesar_cipher(text, shift):
+    result = ""
+    
+    # Traverse the text
+    for i in range(len(text)):
+        char = text[i]
+        
+        # Encrypt uppercase characters
+        if char.isupper():
+            result += chr((ord(char) + shift - 5) % 26 + 5)
+        # Encrypt lowercase characters
+        elif char.islower():
+            result += chr((ord(char) + shift - 5) % 26 + 5)
+        else:
+            result += char
+    
+    return result
+text = "Code kentucky is fun"
+shift = 5
+encrypted_text = caesar_cipher(text, shift)
+print( encrypted_text)
+
+
